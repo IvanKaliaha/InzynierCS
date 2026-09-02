e5dd2fd70698a706842e31ec57c25eb779d7a705
IvanKaliaha <2pochtaru@gmail.com>
2026-09-02 16:32:32 +0200

    Zad2

---
+++ Zad2/Pracowniki/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pracowniki
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Inzynier> inzyniers = new List<Inzynier>
+    {
+        new Inzynier("Andrzej", "Bielewicz", Inzynier.Skutecznosc.SLABY, 2),
+        new Inzynier("Kevin", "Magnussen", Inzynier.Skutecznosc.DOBRY, 4),
+        new Inzynier("Max", "Verstappen", Inzynier.Skutecznosc.WYBITNY, 6)
+    };
+            foreach (var inzynier in inzyniers) {
+                for (int j = 1; j <= 3; j++)
+                {
+                    inzynier.buduj();
+                }
+                inzynier.pokaz();
+                Console.WriteLine();
+            }
+           
+            Inzynier.pokazGlobalneProjekty();
+            Console.ReadKey();
+        }
+    }
+
+}
