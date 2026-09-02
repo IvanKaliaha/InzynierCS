c790125499ecb7fbae673969e1dad075ace5da43
IvanKaliaha <2pochtaru@gmail.com>
2026-09-02 16:12:21 +0200

    Zad1

---
+++ Zad1/Pracowniki/Pracowniki/Inzynier.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pracowniki
+{
+    public class Inzynier
+    {
+       public enum Skutecznosc
+        {
+         SLABY = 1,
+         DOBRY = 2,
+         WYBITNY = 3
+
+
+        }
+        public static int wszystkieProjektyFirmy;
+        public string imie;
+        public string nazwisko;
+        public Skutecznosc skutecznosc;
+        public int liczbaProjektow;
+
+        public Inzynier(string imie, string nazwisko, Skutecznosc skutecznosc, int liczbaProjektow)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.skutecznosc = skutecznosc;
+            this.liczbaProjektow = liczbaProjektow;
+        }
+
+        public void buduj()
+        {
+            int projekty = (int)skutecznosc;
+
+            Console.WriteLine(imie + " " + nazwisko + " buduje " + projekty + " projekty.");
+
+            liczbaProjektow += projekty;
+            wszystkieProjektyFirmy += projekty;
+        }
+
+        public void pokaz()
+        {
+            Console.WriteLine(imie + " " + nazwisko + " ma " + liczbaProjektow + " zrealizowanych projektów.");
+        }
+
+        public static void pokazGlobalneProjekty()
+        {
+            Console.WriteLine("Cała firma zrealizowała: " +
+                              wszystkieProjektyFirmy + " projektów.");
+        }
+    }
+}
---
+++ Zad1/Pracowniki/Pracowniki/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pracowniki
+{
+    class Program
+    {
+        static void Main()
+        {
+            Inzynier Julia = new Inzynier("Julia", "Kowalska", Inzynier.Skutecznosc.Slaby, 3);
+            Inzynier Wiktor = new Inzynier("Wiktor", "Kowalski", Inzynier.Skutecznosc.DOBRY, 4);
+            Inzynier Pawel = new Inzynier("Pawel", "Omarski", Inzynier.Skutecznosc.WYBITNY, 5);
+            Julia.buduj();
+            Julia.pokaz();
+            Pawel.buduj();
+            Pawel.pokaz();
+            Wiktor.buduj();
+            Wiktor.pokaz();
+            Inzynier.pokazGlobalneProjekty();
+            Console.ReadKey();
+        }
+    }
+
+}
